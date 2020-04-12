d9f9351980ddedf53333a5d3ac0861eb672163ef
cafajardop <carlosafp84@hotmail.com>
2020-04-12 12:33:10 -0500

    Terminacion app Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,9 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 
 
 @NgModule({
@@ -26,6 +29,9 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     HomeComponent,
     AboutComponent,
     HeroesComponent,
+    HeroeComponent,
+    BuscadorComponent,
+    HeroeTarjetaComponent,
     
     
   ],
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HeroesService,Heroe } from '../../servicios/heroes.service';
-
+import { HeroesService, Heroe } from '../../servicios/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +8,17 @@ import { HeroesService,Heroe } from '../../servicios/heroes.service';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes:Heroe[]=[];
+  heroes: Heroe[] = [];
 
-  constructor(private _heroesService:HeroesService) { }
+  constructor(private _heroesService: HeroesService,
+    private router: Router) { }
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
-    console.log(this.heroes);
-    
+    // console.log(this.heroes);
   }
 
+  verHeroe(ixd: number) {
+    this.router.navigate(['/heroe', ixd]);
+  }
 }
